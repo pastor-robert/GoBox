2d34cb5712c53e46619a805c1ccdb80c3c14a4cc
Rob Adams <robadams@illinois.edu>
2023-02-07 16:30:17 -0600

    Error handling in greetings.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,14 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // Return a greeting for the named person
-func Hello(name string) string {
-	return fmt.Sprintf("Hi, %v. Welcome!", name)
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	return fmt.Sprintf("Hi, %v. Welcome!", name), nil
 }
