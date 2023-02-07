3a9fc2f06493837f83ee9f9886fae851990dacb1
Rob Adams <robadams@illinois.edu>
2023-02-07 16:14:00 -0600

    Fix capitalization typo

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -4,5 +4,5 @@ import "fmt"
 
 // Return a greeting for the named person
 func Hello(name string) string {
-	return fmt.Sprintf("hi, %v. Welcome!", name)
+	return fmt.Sprintf("Hi, %v. Welcome!", name)
 }
