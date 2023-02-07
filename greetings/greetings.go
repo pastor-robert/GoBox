0019a89c4b6e5b8e7080d70a7b7c0203afabe1df
Rob Adams <robadams@illinois.edu>
2023-02-07 16:41:50 -0600

    Trivial change to test module reloading

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -8,7 +8,7 @@ import (
 // Return a greeting for the named person
 func Hello(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("empty name")
+		return "", errors.New("name not specified")
 	}
 	return fmt.Sprintf("Hi, %v. Welcome!", name), nil
 }
