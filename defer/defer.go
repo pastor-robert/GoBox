d335484c0a73af976343beb3848a38c716f2b9e8
Rob Adams <robadams@illinois.edu>
2023-02-07 19:02:00 -0600

    Add math/rand

--- defer/defer.go
+++ defer/defer.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 import "math/rand"
+import "time"
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	messages := []string{ "a", "b", "c" }
 	fmt.Println("Hello!")
-	for i := 1; i <= 5; i++ {
+	for i := 0; i < 5; i++ {
 		message := messages[rand.Intn(len(messages))]
 		fmt.Printf("%d: %v\n", i, message)
 		if i % 2 == 0 {
