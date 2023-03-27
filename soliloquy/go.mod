490ce64fbbe1ca165c4ef859b360ae7838eb2f49
Rob Adams <robadams@illinois.edu>
2023-03-27 12:55:31 -0500

    Create and populate all three namespaces.

--- soliloquy/go.mod
+++ soliloquy/go.mod
@@ -1,3 +1,22 @@
 module github.com/pastor-robert/GoBox/soliloquy
 
 go 1.18
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/jsimonetti/rtnetlink v1.3.1
+	github.com/milosgajdos/tenus v0.0.3
+	github.com/vishvananda/netlink v1.1.0
+	github.com/vishvananda/netns v0.0.4
+)
+
+require (
+	github.com/docker/libcontainer v2.2.1+incompatible // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/josharian/native v1.0.0 // indirect
+	github.com/mdlayher/netlink v1.7.1 // indirect
+	github.com/mdlayher/socket v0.4.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
--- soliloquy/sol.go
+++ soliloquy/sol.go
@@ -10,17 +10,144 @@ import (
 	"fmt"
 	"io"
 	"log"
+	_ "net"
 	"net/http"
 	"net/url"
 	"os/exec"
 	"time"
+
+	"github.com/google/uuid"
+	_ "github.com/jsimonetti/rtnetlink/rtnl"
+	_ "github.com/milosgajdos/tenus"
+	"github.com/vishvananda/netlink"
+	"github.com/vishvananda/netns"
 )
+
+func linkSetUp( space netns.NsHandle, link netlink.Link, index, host int) {
+	nsOrig, err := netns.Get()
+	if err != nil {
+		log.Fatal("Can't get original netspace: ", err)
+	}
+	defer netns.Set(nsOrig)
+
+	if err := netns.Set(space); err != nil {
+		log.Fatal("Can't switch to namespace: ")
+	}
+
+	addr, _ := netlink.ParseAddr(fmt.Sprintf("10.0.%d.%d/24", index, host))
+	err = netlink.AddrAdd(link, addr)
+	if err != nil {
+		log.Fatal("Can't set IP address: ", err)
+	}
+	err = netlink.LinkSetUp(link)
+	if err != nil {
+		log.Fatal("Can't bring up client address: ", err)
+	}
+}
+
+func createVeth(clientSpace netns.NsHandle, clientIndex int, simSpace netns.NsHandle) {
+	nsOrig, err := netns.Get()
+	if err != nil {
+		log.Fatal("Can't get original netspace: ", err)
+	}
+	defer netns.Set(nsOrig)
+
+
+	err = netns.Set(simSpace)
+	if err != nil {
+		log.Fatal("Can't get original netspace: ", err)
+	}
+
+	// Thanks, https://github.com/teddyking/netsetgo/blob/0.0.1/device/veth.go#L16
+
+	vethLinkAttrs := netlink.NewLinkAttrs()
+	vethLinkAttrs.Name = fmt.Sprintf("client%d", clientIndex)
+
+	veth := &netlink.Veth{
+		LinkAttrs: vethLinkAttrs,
+		PeerName: "sim",
+	}
+
+	if err := netlink.LinkAdd(veth); err != nil {
+		log.Fatal("Can't create veth pair: ", err)
+	}
+
+	// Put the endpoints in the correct spaces
+	simLink, _ := netlink.LinkByName(veth.Name)
+	clientLink, _ := netlink.LinkByName(veth.PeerName)
+
+	// netns.NsHandle is just a file descriptor in disguise
+	err = netlink.LinkSetNsFd(clientLink, int(clientSpace))
+	if err != nil {
+		log.Fatal("Can't put pair into client namespace: ", err)
+	}
+
+	linkSetUp(clientSpace, clientLink, clientIndex, 2)
+	linkSetUp(simSpace, simLink, clientIndex, 1)
+
+	// All done!
+}
+
+func createNSWithLoopback(name, suffix string) netns.NsHandle {
+	nsOrig, err := netns.Get()
+	if err != nil {
+		log.Fatal("Can't get original netspace: ", err)
+	}
+	defer netns.Set(nsOrig)
+
+	// NewNamed leaves us in the new namespace
+	nsHandle, err := netns.NewNamed(name + suffix)
+	if err != nil {
+		log.Fatal("Can't create namespace: ", err)
+	}
+
+	lo, err := netlink.LinkByName("lo")
+	if err != nil {
+		log.Fatal("Can't find loopback: ", err)
+	}
+
+	err = netlink.LinkSetUp(lo)
+	if err != nil {
+		log.Fatal("Can't find loopback: ", err)
+	}
+
+	return nsHandle
+}
+
+// Create all of the required namespaces,
+// veth pairs, and bridges. Assign
+// addresses as appropriate.
+func config() {
+	// TODO completely refactor and generalize
+
+	// So we can distinguish test runs
+	nsSuffix := "-" + uuid.New().String()
+
+	var clientSpaces []netns.NsHandle
+
+	clientSpace := createNSWithLoopback("client0", nsSuffix)
+	clientSpaces = append(clientSpaces, clientSpace)
+
+	clientSpace = createNSWithLoopback("client1", nsSuffix)
+	clientSpaces = append(clientSpaces, clientSpace)
+
+	simSpace := createNSWithLoopback("sim", nsSuffix)
+
+	// For each client, create a veth pair between
+	// it and the sim
+	for i, clientSpace := range(clientSpaces) {
+		createVeth(clientSpace, i, simSpace)
+	}
+}
+
 
 // Run an http server in the background, then
 // run a set of http clients in the foreground.
 // The server prints the message that the client sent.
 // The client prints the message that the server sent.
 func main() {
+	config()
+	return
 	fmt.Println("yo, wo")
 	go server()
 
@@ -35,13 +162,13 @@ func main() {
 	}
 	for i := 0; i < 500; i++ {
 		u.RawQuery = url.Values{
-			"i": {fmt.Sprint(i)},
+			"i":   {fmt.Sprint(i)},
 			"msg": {"Hello from client"},
 		}.Encode()
 		clientInternal(u)
 
 		u.RawQuery = url.Values{
-			"i": {fmt.Sprint(i*100)},
+			"i":   {fmt.Sprint(i * 100)},
 			"msg": {"Hello from cURL"},
 		}.Encode()
 		clientExternal(u)
@@ -74,7 +201,7 @@ func clientExternal(u url.URL) {
 	fmt.Printf("X %s\n", out)
 }
 
-// Launch a web server on "http://0.0.0.0:8000/". For each request, 
+// Launch a web server on "http://0.0.0.0:8000/". For each request,
 // decode the parameters and return a message in the response.
 func server() {
 	http.HandleFunc(
